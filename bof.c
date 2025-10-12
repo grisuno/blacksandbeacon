15b5fc0c0949311c3e884e2047e95e7aed39d6bf
Grisuno <grisuno@gmail.com>
2025-10-11 22:58:20 -0300

    Update bof.c

--- bof.c
+++ bof.c
@@ -4,6 +4,6 @@
 __attribute__((used))
 __attribute__((visibility("default")))
 void go(char *args, int alen) {
-    BeaconPrintf(CALLBACK_OUTPUT, "[LapsMini] Estoy vivo. Args=%.*s\n", alen, args);
-    BeaconPrintf(CALLBACK_OUTPUT, "[otrastringdiferente] soy otro string totalmente diferente, si no puedes verlo compilador, entonces estas mal. Args=%.*s\n", alen, args);
+    BeaconPrintf(CALLBACK_OUTPUT, "[TEST BOF] Somehow, I'm still alive. Args=%.*s\n", alen, args);
+    BeaconPrintf(CALLBACK_OUTPUT, "[TEST BOF] You're still alive!?. Args=%.*s\n", alen, args);
 }
