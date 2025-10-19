b9b8c35fa498d3eb0747884ead03039471cfa4af
Grisuno <grisuno@gmail.com>
2025-10-19 01:24:29 -0300

    Modify gcc command to include dynamic linking and new target

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 apt-get install gcc libcurl4-openssl-dev libssl-dev
-gcc -o beacon beacon3.c aes.c cJSON.c -lcurl -lssl -lcrypto
+gcc -rdynamic -o beacon beacon3.c aes.c cJSON.c -lcurl -lssl -lcrypto
+gcc -rdynamic -o beacon_gopher gopher_beacon.c aes.c cJSON.c -lcurl -lssl -lcrypto
